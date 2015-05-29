4530947934fa48f8cc3d725a742190a8539d398a
Wodric <alexandre.lefevre18@gmail.com>
2015-05-29 22:48:39 +0200

    [DEV #4] Begin to parametrize the UI message with new framework

--- tournamentUI/tournamentUI-ui/src/main/java/com/plm/MyUI.java
+++ tournamentUI/tournamentUI-ui/src/main/java/com/plm/MyUI.java
@@ -1,10 +1,14 @@
 package com.plm;
 
+import java.util.Locale;
+
 import javax.servlet.annotation.WebServlet;
 
-import com.plm.tournament.structures.blinds.BlindStructureMainInformationPanel;
+import org.atmosphere.cache.SessionBroadcasterCache;
+
+import com.plm.internationalization.ParametrizedResourceBundle;
+import com.plm.messages.constants.MessagesConstants;
 import com.plm.tournament.structures.blinds.BlindStructurePresenter;
-import com.plm.tournament.structures.blinds.BlindStructureView;
 import com.plm.tournament.structures.blinds.BlindStructureViewImpl;
 import com.plm.tournamentCore.blind.BlindStructure;
 import com.vaadin.annotations.Theme;
@@ -14,7 +18,6 @@ import com.vaadin.navigator.Navigator;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
 
 /**
  *
@@ -30,9 +33,15 @@ public class MyUI extends UI {
 	
     Navigator navigator;
     
+    private static Locale userLocale; 
+    
 	@Override
     protected void init(VaadinRequest vaadinRequest) {
-        this.getPage().setTitle("Navigation Example");
+		userLocale = vaadinRequest.getLocale();
+		ParametrizedResourceBundle bundle = ParametrizedResourceBundle.
+				getParametrizedBundle(MessagesConstants.UI_MESSAGE_FILE_BASE_NAME, userLocale);
+        this.getPage().setTitle(
+        		bundle.getMessage(MessagesConstants.PAGE_MAIN_TITLE));
         
         BlindStructureViewImpl createStructureView = new BlindStructureViewImpl();
         BlindStructure structureModel = createStructureView.getStructureToDisplayFromModel();
@@ -43,6 +52,10 @@ public class MyUI extends UI {
         
         new BlindStructurePresenter(structureModel,createStructureView);
     }
+	
+	public static Locale getUserLocale(){
+		return userLocale;
+	}
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
---
+++ tournamentUI/tournamentUI-ui/src/main/java/com/plm/messages/constants/MessagesConstants.java
@@ -0,0 +1,40 @@
+package com.plm.messages.constants;
+
+public class MessagesConstants {
+	
+	/*
+	 * File name
+	 */
+	
+	public static final String UI_MESSAGE_FILE_BASE_NAME = "ui-messages";
+	
+	/*
+	 * file constant name 
+	 */
+	
+	/**
+	 * Constant to find the main page title on ui-messages files
+	 */
+	public static final String PAGE_MAIN_TITLE = "page.main.title";
+	
+	/**
+	 * Constant to find the ante column header of blind structure grid
+	 */
+	public static final String BLINDSTRUCTURE_GRID_COLUMN_HEADER_ANTE = "blindStructure.grid.column.header.ante";
+	
+	/**
+	 * Constant to find the big blind column header of blind structure grid
+	 */
+	public static final String BLINDSTRUCTURE_GRID_COLUMN_HEADER_BIGBLIND = "blindStructure.grid.column.header.bigBlind";
+	
+	/**
+	 * Constant to find the duration column header of blind structure grid
+	 */
+	public static final String BLINDSTRUCTURE_GRID_COLUMN_HEADER_DURATION = "blindStructure.grid.column.header.duration";
+	
+	/**
+	 * Constant to find the small blind column header of blind structure grid
+	 */
+	public static final String BLINDSTRUCTURE_GRID_COLUMN_HEADER_SMALLBLIND = "blindStructure.grid.column.header.smallBlind";
+
+}
