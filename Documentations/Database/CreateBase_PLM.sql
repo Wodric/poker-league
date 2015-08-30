0d0cb0d6dc9ae33befae99fa96c638f97941f4ef
Alexandre Lefevre <alexandre.lefevre18@gmail.com>
2015-08-30 21:22:17 +0200

    [DEV #24] add user, roles and permissions tables on schema and update create base script

--- Documentations/Database/CreateBase_PLM.sql
+++ Documentations/Database/CreateBase_PLM.sql
@@ -8,54 +8,155 @@ USE `PokerLeagueManager` ;
 -- -----------------------------------------------------
 -- Table `PokerLeagueManager`.`blindStructure`
 -- -----------------------------------------------------
-CREATE TABLE `blindStructure` (
-  `idblindStructure` bigInt(20) NOT NULL AUTO_INCREMENT,
-  `structureJson` longtext,
-  PRIMARY KEY (`idblindStructure`),
-  UNIQUE KEY `idblindStructure_UNIQUE` (`idblindStructure`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `blindStructure` (
+  `blindStructureId` BIGINT NOT NULL AUTO_INCREMENT,
+  `jsonStructure` LONGTEXT NULL,
+  PRIMARY KEY (`blindStructureId`),
+  UNIQUE INDEX `idblindStructure_UNIQUE` (`blindStructureId` ASC))
+ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;
 
 -- -----------------------------------------------------
 -- Table `PokerLeagueManager`.`legalFee`
 -- -----------------------------------------------------
-CREATE TABLE `legalFee` (
-  `idFee` bigInt(20) NOT NULL,
-  `shortName` varchar(45) NOT NULL,
-  `description` varchar(45) NOT NULL,
-  `feePercent` int(11) DEFAULT NULL,
-  `feeFixed` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idFee`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `legalFee` (
+  `feeId` BIGINT NOT NULL,
+  `shortName` VARCHAR(50) NOT NULL,
+  `description` VARCHAR(200) NOT NULL,
+  `feePourcent` INT NULL,
+  `feeFixed` INT NULL,
+  PRIMARY KEY (`feeId`))
+ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- -----------------------------------------------------
 -- Table `PokerLeagueManager`.`tournament`
 -- -----------------------------------------------------
-CREATE TABLE `tournament` (
-  `idTournament` bigInt(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) DEFAULT NULL,
-  `description` text,
-  `variante` varchar(20) DEFAULT NULL,
-  `maxPlayer` int(11) DEFAULT NULL,
-  `isRebuy` tinyint(4) DEFAULT '0',
-  `isDoubleRebuyAuthorized` tinyint(4) DEFAULT '0',
-  `isShootout` tinyint(4) DEFAULT '0',
-  `isQualifier` tinyint(4) DEFAULT '0',
-  `isReentry` tinyint(4) DEFAULT '0',
-  `buyIn` int(11) DEFAULT NULL,
-  `rake` int(11) DEFAULT NULL,
-  `startingChips` int(11) DEFAULT NULL,
-  `addOnChips` int(11) DEFAULT NULL,
-  `rebuyChips` int(11) DEFAULT NULL,
-  `blindStructure_idblindStructure` bigInt(20) NOT NULL,
-  `legalFeeId` bigInt(20) NOT NULL,
-  PRIMARY KEY (`idTournament`,`blindStructure_idblindStructure`,`legalFeeId`),
-  UNIQUE KEY `idTournament_UNIQUE` (`idTournament`),
-  KEY `fk_tournament_blindStructure_idx` (`blindStructure_idblindStructure`),
-  KEY `fk_tournament_legalFee_idx` (`legalFeeId`),
-  CONSTRAINT `fk_tournament_blindStructure` FOREIGN KEY (`blindStructure_idblindStructure`) REFERENCES `blindStructure` (`idblindStructure`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_tournament_legalFee` FOREIGN KEY (`legalFeeId`) REFERENCES `legalFee` (`idFee`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `PokerLeagueManager`.`tournament` (
+  `idTournament` BIGINT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) CHARACTER SET 'utf8' NULL,
+  `description` TEXT NULL,
+  `variante` VARCHAR(20) NULL,
+  `maxPlayer` INT NULL,
+  `isRebuy` TINYINT NULL DEFAULT 0,
+  `isDoubleRebuyAuthorized` TINYINT NULL DEFAULT 0,
+  `isShootout` TINYINT NULL DEFAULT 0,
+  `isQualifier` TINYINT NULL DEFAULT 0,
+  `isReentry` TINYINT NULL DEFAULT 0,
+  `buyIn` INT NULL,
+  `rake` INT NULL,
+  `startingChips` INT NULL,
+  `addOnChips` INT NULL,
+  `rebuyChips` INT NULL,
+  `fk_feeId` BIGINT NULL,
+  `fk_blindStructureId` BIGINT NOT NULL,
+  PRIMARY KEY (`idTournament`),
+  UNIQUE INDEX `idTournament_UNIQUE` (`idTournament` ASC),
+  INDEX `fk_tournament_blindStructure_idx` (`fk_blindStructureId` ASC),
+  INDEX `fk_tournament_legalFee_idx` (`fk_feeId` ASC),
+  CONSTRAINT `fk_tournament_blindStructure`
+    FOREIGN KEY (`fk_blindStructureId`)
+    REFERENCES `blindStructure` (`blindStructureId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_tournament_legalFee`
+    FOREIGN KEY (`fk_feeId`)
+    REFERENCES `legalFee` (`feeId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;
 
+
+-- -----------------------------------------------------
+-- Table `PokerLeagueManager`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `user` (
+  `userId` BIGINT NOT NULL,
+  `username` VARCHAR(32) CHARACTER SET 'utf8' NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `password` TEXT NOT NULL,
+  `passwordSalt` TEXT NOT NULL,
+  `active` TINYINT(1) NULL DEFAULT 0,
+  `verified` TINYINT(1) NULL DEFAULT 0,
+  `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `passwordModificationTime` TIMESTAMP NULL,
+  PRIMARY KEY (`userId`))
+ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- -----------------------------------------------------
+-- Table `PokerLeagueManager`.`userInformation`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `userInformation` (
+  `userInformationId` BIGINT NOT NULL,
+  `user_userId` BIGINT NOT NULL,
+  PRIMARY KEY (`userInformationId`, `user_userId`),
+  INDEX `fk_userInformation_user1_idx` (`user_userId` ASC),
+  CONSTRAINT `fk_userInformation_user1`
+    FOREIGN KEY (`user_userId`)
+    REFERENCES `PokerLeagueManager`.`user` (`userId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- -----------------------------------------------------
+-- Table `PokerLeagueManager`.`role`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `role` (
+  `roleId` BIGINT NOT NULL,
+  `roleName` VARCHAR(45) NULL,
+  PRIMARY KEY (`roleId`))
+ENGINE = InnoDB DEFAULT CHARSET=utf8;
+
+-- -----------------------------------------------------
+-- Table `PokerLeagueManager`.`permission`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `permission` (
+  `idpermission` INT NOT NULL,
+  `permission` VARCHAR(45) NULL,
+  PRIMARY KEY (`idpermission`))
+ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;
+
+-- -----------------------------------------------------
+-- Table `PokerLeagueManager`.`userRoles`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `userRoles` (
+  `fk_userId` BIGINT NOT NULL,
+  `fk_roleId` BIGINT NOT NULL,
+  PRIMARY KEY (`fk_userId`, `fk_roleId`),
+  INDEX `fk_user_has_role_role1_idx` (`fk_roleId` ASC),
+  INDEX `fk_user_has_role_user1_idx` (`fk_userId` ASC),
+  CONSTRAINT `fk_user_has_role_user1`
+    FOREIGN KEY (`fk_userId`)
+    REFERENCES `user` (`userId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_role_role1`
+    FOREIGN KEY (`fk_roleId`)
+    REFERENCES `role` (`roleId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;
+
+-- -----------------------------------------------------
+-- Table `PokerLeagueManager`.`rolePermissions`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `rolePermissions` (
+  `fk_roleId` BIGINT NOT NULL,
+  `fk_permissionId` INT NOT NULL,
+  PRIMARY KEY (`fk_roleId`, `fk_permissionId`),
+  INDEX `fk_role_has_permission_permission1_idx` (`fk_permissionId` ASC),
+  INDEX `fk_role_has_permission_role1_idx` (`fk_roleId` ASC),
+  CONSTRAINT `fk_role_has_permission_role1`
+    FOREIGN KEY (`fk_roleId`)
+    REFERENCES `role` (`roleId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_role_has_permission_permission1`
+    FOREIGN KEY (`fk_permissionId`)
+    REFERENCES `permission` (`idpermission`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
